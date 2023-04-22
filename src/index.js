89b6e9cfd48a9adfc0ca0ce3e9a71af15920d0d8
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-04-22 12:45:49 -0300

    Getting the parsed rax data from the openAI

--- src/index.js
+++ src/index.js
@@ -110,15 +110,12 @@ app.post("/api/recipe", (req, res) => {
     .post("https://api.openai.com/v1/completions", params)
     .then((result) => {
       console.log('Index.js line 112 raw data from OpenAi', result);
-  
-      //tester code
-/*
-      const recipeTestInstructions = recipeLines
-  .slice(instructionsStartIndex + 1, caloriesStartIndex)
-  .filter((line) => line.trim().length > 0);
-      console.log('index.js line 113 Processed recipeInstructions:', recipeTestInstructions);
-*/
+        
       const recipeText = result.data.choices[0].text;
+             
+      //tester code
+      console.log('Index.js line 117 Raw recipe data:', recipeText); // Added line to print raw recipe data
+
       const recipeLines = recipeText.split("\n").filter((line) => line.trim().length > 0);
       const recipeName = recipeLines.shift();
       const ingredientsStartIndex = recipeLines.findIndex((line) => line.includes("Ingredients:"));
